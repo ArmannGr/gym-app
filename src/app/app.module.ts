6d870f3d688570a85a928b507072184f3602615a
ArmannGr <grewal.armann15@gmail.com>
2022-12-25 19:53:06 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { WorkoutComponent } from './workout/workout.component';
+import { AppRoutingModule } from './app-routing.module';
+import {RouterOutlet} from "@angular/router";
+import {MatInputModule} from '@angular/material/input';
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    WorkoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    AppRoutingModule,
+    RouterOutlet,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
